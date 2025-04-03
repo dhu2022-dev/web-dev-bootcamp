2e5da28a276b678385814e6e8d0df8ebc54cd5ee
David Hu <who.is.david101@gmail.com>
2025-04-03 18:16:01 -0400

    281 Complete. Also updated gitignore.

---
+++ React/281-javascript-expressions-in-jsx/src/index.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const name = "David";
+const luckyNumber = 8;
+
+ReactDOM.render(
+    <div>
+        <h1>Hello {name}!</h1>
+        <h2>Your lucky number is {luckyNumber}</h2>
+    </div>,
+    document.getElementById("root"));
+
+// If you're running this locally in VS Code use the commands:
+// npm install
+// to install the node modules and
+// npm run dev
+// to launch your react project in your browser
