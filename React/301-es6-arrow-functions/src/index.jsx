c9b96bfdb96b9217247e952235fb31fbe26ed70a
David Hu <who.is.david101@gmail.com>
2025-04-20 15:57:51 -0400

    Challenge 301 Complete

--- React/301-es6-arrow-functions/src/index.jsx
+++ React/301-es6-arrow-functions/src/index.jsx
@@ -4,32 +4,57 @@ import App from "./components/App";
 
 ReactDOM.render(<App />, document.getElementById("root"));
 
-// var numbers = [3, 56, 2, 48, 5];
+var numbers = [3, 56, 2, 48, 5];
 
 ////Map -Create a new array by doing something with each item in an array.
-// const newNumbers = numbers.map(function (x) {
-//   return x * 2;
-// });
+const mapNumbers = numbers.map(function (x) {
+  return x * 2;
+});
+
+const mapNumbers2 = numbers.map(x => x * 2);
+
+console.log(mapNumbers);
+console.log(mapNumbers2);
 
 //////Filter - Create a new array by keeping the items that return true.
-// const newNumbers = numbers.filter(function(num) {
-//   return num < 10;
-// });
+const filterNumbers = numbers.filter(function(num) {
+  return num < 10;
+});
+
+const filterNumbers2 = numbers.filter(num => num < 10);
+
+console.log(filterNumbers);
+console.log(filterNumbers2);
 
 //Reduce - Accumulate a value by doing something to each item in an array.
-// var newNumber = numbers.reduce(function (accumulator, currentNumber) {
-//     return accumulator + currentNumber;
-// })
+var reduceNumber = numbers.reduce(function (accumulator, currentNumber) {
+    return accumulator + currentNumber;
+})
+
+var reduceNumber2 = numbers.reduce((accumulator, currentNumber) => accumulator + currentNumber);
+
+console.log(reduceNumber);
+console.log(reduceNumber2);
 
 ////Find - find the first item that matches from an array.
-// const newNumber = numbers.find(function (num) {
-//   return num > 10;
-// })
+const findNumber = numbers.find(function (num) {
+  return num > 10;
+})
+
+const findNumber2 = numbers.find(num => num > 10);
+
+console.log(findNumber);
+console.log(findNumber2);
 
 ////FindIndex - find the index of the first item that matches.
-// const newNumber = numbers.findIndex(function (num) {
-//   return num > 10;
-// })
+const findIndexNumber = numbers.findIndex(function (num) {
+  return num > 10;
+})
+
+const findIndexNumber2 = numbers.findIndex(num => num > 10);
+
+console.log(findIndexNumber);
+console.log(findIndexNumber2);
 
 // If you're running this locally in VS Code use the commands:
 // npm install
